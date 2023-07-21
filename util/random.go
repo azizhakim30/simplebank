cc781dcf5b70f89a1643d09696c56c3959e2b2d7
Aziz <aziz.hkm30@gmail.com>
2023-07-21 14:31:32 +0700

    mockdb

--- util/random.go
+++ util/random.go
@@ -37,7 +37,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"IDR"}
+	currencies := []string{"IDR", "USD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
